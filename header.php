b2808738d888058300deee536042f2186b027cb3
saidboude <e9875172@cmaisonneuve.qc.ca>
2022-07-09 11:31:00 -0400

    31w-cours ajout le logo de maisonneuve

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+ /**
+  * Template header
+  * Ve template est intégré dans tous les modèles de thème
+  * @link : https://developer.wordpress.org/themes/
+  * @package : Mon WordPress
+  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>  >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section class="site">
+        <header class="site__header">
+            <section class="site__branding">
+                <h1>
+                    <div>
+                    <img scr="2022-31w/wp-content/themes/31w-cours/logo.jpg" alt="logo">
+                    </div>
+                    <a href="<?=esc_url(home_url('/'))?>"><?=bloginfo('name');?></a>
+                    
+                   
+                </h1>
+                <h2>
+                    <?=bloginfo('description');?>
+                </h2>
+                </section>
+            <nav class="site__navigation">
+            Menu
+            </nav>
+        </header>
+        <section class="site__sidebar">
+            <h3>Sidebar</h3>
+        </section>
